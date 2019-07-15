ee1e2716a27eb628795bb5b67274215c3374c5d8
bingwumeihuo <1183223389@qq.com>
2019-07-16 01:15:45 +0800

    新增楼盘微服务

--- yizu-estate/src/main/java/com/yizu/estate/entity/Estate.java
+++ yizu-estate/src/main/java/com/yizu/estate/entity/Estate.java
@@ -17,7 +17,8 @@ import java.io.Serializable;
 public class Estate implements Serializable{
 
 	@Id
-	private Long id;//id
+	//id
+	private Long id;
 
 
 	//楼盘名称
@@ -46,96 +47,7 @@ public class Estate implements Serializable{
 	private java.util.Date updated;
 
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getProvince() {
-		return province;
-	}
-	public void setProvince(String province) {
-		this.province = province;
-	}
-
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getArea() {
-		return area;
-	}
-	public void setArea(String area) {
-		this.area = area;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getYear() {
-		return year;
-	}
-	public void setYear(String year) {
-		this.year = year;
-	}
-
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getPropertyCost() {
-		return propertyCost;
-	}
-	public void setPropertyCost(String propertyCost) {
-		this.propertyCost = propertyCost;
-	}
-
-	public String getPropertyCompany() {
-		return propertyCompany;
-	}
-	public void setPropertyCompany(String propertyCompany) {
-		this.propertyCompany = propertyCompany;
-	}
-
-	public String getDevelopers() {
-		return developers;
-	}
-	public void setDevelopers(String developers) {
-		this.developers = developers;
-	}
-
-	public java.util.Date getCreated() {
-		return created;
-	}
-	public void setCreated(java.util.Date created) {
-		this.created = created;
-	}
-
-	public java.util.Date getUpdated() {
-		return updated;
-	}
-	public void setUpdated(java.util.Date updated) {
-		this.updated = updated;
-	}
 
 
 	
