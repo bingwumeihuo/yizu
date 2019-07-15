c9f8fc01045f1768d68fe251daa15ba429314079
bingwumeihuo <1183223389@qq.com>
2019-07-16 00:29:13 +0800

    新增房源微服务

--- yizu-estate/src/main/java/com/yizu/estate/dao/EstateDao.java
+++ yizu-estate/src/main/java/com/yizu/estate/dao/EstateDao.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.yizu.estate.entity.Estate;
+
+import java.util.List;
+
 /**
  * estate数据访问接口
  * @author Administrator
  *
  */
 public interface EstateDao extends JpaRepository<Estate,Long>,JpaSpecificationExecutor<Estate>{
-	
+
+    void deleteById(List<Long> ids);
 }
--- yizu-estate/src/main/java/com/yizu/estate/service/EstateService.java
+++ yizu-estate/src/main/java/com/yizu/estate/service/EstateService.java
@@ -8,6 +8,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
 
 import com.yizu.common.util.IdWorker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,7 @@ import com.yizu.estate.entity.Estate;
  * @author Administrator
  *
  */
+@Transactional
 @Service
 public class EstateService {
 
@@ -102,6 +104,9 @@ public class EstateService {
 	 */
 	public void deleteById(Long id) {
 		estateDao.deleteById(id);
+	}
+	public void deleteByIds(List<Long> ids) {
+		estateDao.deleteById(ids);
 	}
 
 	/**
